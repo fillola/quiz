645661c95e4e974b30aaee25a690989992350654
fillola <jose.fillola@gmail.com>
2015-07-30 01:45:54 +0200

    modificaciones

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,8 @@
 // Definición del modelo de Quiz
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Quiz', 
-                          { pregunta: DataType.STRING, 
-                            pregunta: DataType.STRING }); 
+                          { pregunta: DataTypes.STRING, 
+                            respuesta: DataTypes.STRING 
+                          }); 
   
-}};
+}
