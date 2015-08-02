42a972973657ae713f2ef87eeb955b4344c242b5
fillola <jose.fillola@gmail.com>
2015-08-02 19:08:51 +0200

    modificaciones

--- models/models.js
+++ models/models.js
@@ -1,5 +1,8 @@
 var path = require('path');
 
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
@@ -12,8 +15,7 @@ var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
-// Cargar Modelo ORM
-var Sequelize = require('sequelize');
+
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd, 
