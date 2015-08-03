8d8f7322d019f5ddf38ba7a7e0468c6c6a219b50
fillola <jose.fillola@gmail.com>
2015-08-03 19:59:28 +0200

    dsds

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,6 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/search', quizController.search);
-
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
 
